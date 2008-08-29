0df62cf21e12ceb38d4d2f40125540dcdbea7d1f
calvinlough <calvinlough@abd3d5e1-6655-0410-b14e-df5c2ad5c991>
2008-08-29 02:42:19 +0000

    New class for SQL

---
+++ includes/sql.php
@@ -0,0 +1,20 @@
+<?php
+/*
+
+SQL Buddy - Web based MySQL administration
+http://www.sqlbuddy.com/
+
+gettext.php
+- translation file reader
+
+MIT license
+
+2008 Calvin Lough <http://calv.in>
+
+*/
+
+class SQL {
+	
+	
+	
+}
