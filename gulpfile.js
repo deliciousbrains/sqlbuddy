041ed0a2fe87380f3b0b88af0f51b19947d376fc
Gilbert Pellegrom <gilbert@pellegrom.me>
2016-06-03 17:21:38 +0100

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.browserify('app.js')
-       .version(['js/app.js']);
+	mix.browserify('app.js')
+		.sass(['app.scss'], 'public/css')
+		.version(['js/app.js', 'css/app.css']);
 });
