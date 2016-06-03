d23d02b926c66ae546fc5fcf3531a70fceae3c6a
Gilbert Pellegrom <gilbert@pellegrom.me>
2016-06-03 17:20:19 +0100

    Create ApiController

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class ApiController extends Controller
+{
+    /**
+     * @param \Exception $e
+     * @return array
+     */
+    protected function getError(\Exception $e)
+    {
+        $result = [
+            'error' => $e->getMessage(),
+        ];
+
+        if (env('APP_DEBUG')) {
+            $result['stack_trace'] = explode("\n", $e->getTraceAsString());
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param string $databaseName
+     */
+    protected function selectDatabase($databaseName)
+    {
+        $pdo = \DB::connection()->getPdo();
+        $pdo->exec('use ' . $databaseName);
+    }
+}
