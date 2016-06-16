e926a1d88c3a032d8274dd6a816f6ea4ef71f4ca
Ashley Rich <hello@ashleyrich.com>
2016-06-16 13:39:30 +0100

    Pagination now uses next and previous links

--- resources/assets/js/util/pagination.js
+++ resources/assets/js/util/pagination.js
@@ -23,13 +23,23 @@ Vue.component('pagination', {
 	},
 
 	template: '<ul class="pagination" v-if="data.total > data.per_page">' +
-	'	<li class="page-item" v-if="data.prev_page_url">' +
-	'		<a href="#" class="page-link" aria-label="Previous" @click.prevent="selectPage(--this.data.current_page)"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a>' +
-	'	</li>' +
-	'	<li class="page-item" v-for="n in data.last_page" :class="{ \'active\': n + 1 == data.current_page }"><a href="#" class="page-link" @click.prevent="selectPage(n + 1)">{{ n + 1 }}</a></li>' +
-	'	<li class="page-item" v-if="data.next_page_url">' +
-	'		<a href="#" class="page-link" aria-label="Next" @click.prevent="selectPage(++this.data.current_page)"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a>' +
-	'	</li>' +
+	'	<template v-if="data.prev_page_url">' +
+	'		<li class="page-item">' +
+	'			<a href="#" class="page-link" aria-label="First" @click.prevent="selectPage(1)"><span aria-hidden="true">&laquo;</span><span class="sr-only">First</span></a>' +
+	'		</li>' +
+	'		<li class="page-item">' +
+	'			<a href="#" class="page-link" aria-label="Previous" @click.prevent="selectPage(--this.data.current_page)"><span aria-hidden="true">&lsaquo;</span><span class="sr-only">Previous</span></a>' +
+	'		</li>' +
+	'	</template>' +
+
+	'	<template v-if="data.next_page_url">' +
+	'		<li class="page-item">' +
+	'			<a href="#" class="page-link" aria-label="Next" @click.prevent="selectPage(++this.data.current_page)"><span aria-hidden="true">&rsaquo;</span><span class="sr-only">Next</span></a>' +
+	'		</li>' +
+	'		<li class="page-item">' +
+	'			<a href="#" class="page-link" aria-label="Last" @click.prevent="selectPage(this.data.last_page)"><span aria-hidden="true">&raquo;</span><span class="sr-only">Last</span></a>' +
+	'		</li>' +
+	'	</template>' +
 	'</ul>',
 
 	methods: {
