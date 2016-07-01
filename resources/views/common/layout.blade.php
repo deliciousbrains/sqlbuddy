593e12ed215c2cca1ef3d606db3873d1493e8783
Gilbert Pellegrom <gilbert@pellegrom.me>
2016-07-01 10:54:48 +0100

    Add Bootstrap dependency

--- resources/views/common/layout.blade.php
+++ resources/views/common/layout.blade.php
@@ -37,6 +37,7 @@
     </sqlbuddy>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.2/js/tether.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js"
             integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7"
             crossorigin="anonymous"></script>
