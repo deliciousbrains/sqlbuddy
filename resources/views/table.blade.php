40a5f93c965e29a5c4c2b2707160b4b8fff7c32c
Gilbert Pellegrom <gilbert@pellegrom.me>
2016-07-01 11:05:29 +0100

    Show placeholders when no table selected or no data

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -3,22 +3,33 @@
 @section('content')
 	<sqlbuddy-table :selected-database="selectedDatabase" :selected-table="selectedTable" inline-template>
 		<div class="sqlbuddy-table">
-			<div class="sqlbuddy-table-table-wrapper">
-				<table class="table">
-					<thead>
-						<tr>
-							<th v-for="column in columns">@{{ column }}</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr v-for="row in rows.data">
-							<td v-for="column in columns">@{{ row[column] }}</td>
-						</tr>
-					</tbody>
-				</table>
+			<div v-if="columns.length">
+				<div class="sqlbuddy-table-table-wrapper">
+					<table class="table">
+						<thead>
+							<tr>
+								<th v-for="column in columns">@{{ column }}</th>
+							</tr>
+						</thead>
+						<tbody v-if="rows.data.length">
+							<tr v-for="row in rows.data">
+								<td v-for="column in columns">@{{ row[column] }}</td>
+							</tr>
+						</tbody>
+						<tbody v-else>
+							<tr>
+								<td class="text-xs-center" colspan="@{{ columns.length }}">No data.</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<pagination name="rows" :data="rows"></pagination>
 			</div>
-
-			<pagination name="rows" :data="rows"></pagination>
+			<div class="card text-xs-center" v-else>
+				<div class="card-block">
+					Select a table.
+				</div>
+			</div>
 		</div>
 	</sqlbuddy-table>
 @endsection
