c3f1f4005864f704b17221e7efb8391418b1e063
Gilbert Pellegrom <gilbert@pellegrom.me>
2016-07-01 12:37:34 +0100

    Update bootstrap.js

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,14 +1,15 @@
 // VueJS
 if (window.Vue === undefined) {
-    window.Vue = require('vue');
+	window.Vue = require('vue');
 }
 
 Vue.use(require('vue-resource'));
+
+// Utils
+require('./util/truncated');
+Vue.component('pagination', require('./util/pagination.vue'));
 
 // Components
 require('./components/sqlbuddy');
 require('./components/sidebar');
 require('./components/table');
-
-// Utils
-Vue.component('pagination', require('./util/pagination.vue'));
