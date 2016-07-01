6d823f591aa6a3e713d6c9c373726a6e5950c0e2
Gilbert Pellegrom <gilbert@pellegrom.me>
2016-07-01 11:06:18 +0100

    Reset table when changing database or error

--- resources/assets/js/components/table.js
+++ resources/assets/js/components/table.js
@@ -7,18 +7,12 @@ Vue.component('sqlbuddy-table', {
 	data() {
 		return {
 			columns: [],
-			rows: {
-				current_page: 1,
-				data: [],
-				from: 1,
-				last_page: 1,
-				next_page_url: null,
-				per_page: 10,
-				prev_page_url: null,
-				to: 1,
-				total: 1
-			}
+			rows: {}
 		}
+	},
+
+	ready() {
+		this.resetTableData();
 	},
 
 	methods: {
@@ -30,16 +24,35 @@ Vue.component('sqlbuddy-table', {
 			this.$http.get('/api/databases/' + encodeURI(this.selectedDatabase) + '/tables/' + encodeURI(table) + '?page=' + page)
 				.then(response => {
 					if (response.data.error) {
+						this.resetTableData();
 						this.$dispatch('error', response.data.error);
 					} else {
 						this.columns = response.data.columns;
 						this.rows = response.data.rows;
 					}
 				});
+		},
+
+		resetTableData() {
+			this.columns = [];
+			this.rows = {
+				current_page: 1,
+				data: [],
+				from: 1,
+				last_page: 1,
+				next_page_url: null,
+				per_page: 10,
+				prev_page_url: null,
+				to: 1,
+				total: 1
+			};
 		}
 	},
 
 	watch: {
+		'selectedDatabase': function(database) {
+			this.resetTableData();
+		},
 		'selectedTable': function(table) {
 			this.getTableRows(table);
 		}
