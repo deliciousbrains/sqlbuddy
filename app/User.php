4a790a61f66b1be902ceb5aecb09c459e9f1f691
Gilbert Pellegrom <gilbert@pellegrom.me>
2016-10-14 17:09:08 +0100

    Make User class extend GenericUser

--- app/User.php
+++ app/User.php
@@ -2,31 +2,12 @@
 
 namespace App;
 
-use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\GenericUser;
 use Laravel\Lumen\Auth\Authorizable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends GenericUser implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email',
-    ];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
+    use Authorizable;
 }
