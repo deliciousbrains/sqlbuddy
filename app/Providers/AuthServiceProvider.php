3f571a8a74d253e8371737cf87f31fb4bfd4905a
Gilbert Pellegrom <gilbert@pellegrom.me>
2017-02-20 16:36:29 +0000

    Remove first implementation

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,19 +25,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \Auth::provider('session', function ($app, array $config) {
-            $app->make('hash');
-
             // Return an instance of Illuminate\Contracts\Auth\UserProvider...
-            return new SessionUserProvider($this->app['hash'], $config);
-        });
-
-        \Auth::extend('database', function ($app, $name, array $config) {
-            $app->configure('database');
-            $app->make('db');
-
-            // Return an instance of Illuminate\Contracts\Auth\Guard...
-            return new DatabaseGuard(\Auth::createUserProvider($config['provider'], $app['db.factory'], $app['config']['database']));
         });
     }
 }
